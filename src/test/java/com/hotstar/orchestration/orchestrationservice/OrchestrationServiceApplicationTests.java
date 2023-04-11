923204b27e4dc87063056b0438a1bb5216074f4a
Harsh Shah <harshshah@NOVI-C02GH2N7MD6M.local>
2023-04-11 06:42:41 +0530

    first commit

---
+++ src/test/java/com/hotstar/orchestration/orchestrationservice/OrchestrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotstar.orchestration.orchestrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
