e7aad3816df1422712d56bd6d94fc7f4ffdd891a
Harsh Shah <harshshah@NOVI-C02GH2N7MD6M.local>
2023-04-14 11:09:04 +0530

    service

--- src/main/java/com/hotstar/orchestration/service/Impl/PathFinderEdgeImpl.java
+++ src/main/java/com/hotstar/orchestration/service/Impl/PathFinderEdgeImpl.java
@@ -3,8 +3,11 @@ package com.hotstar.orchestration.service.Impl;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import com.hotstar.orchestration.service.PathFinderEdge;
 
+@Service
 public class PathFinderEdgeImpl implements PathFinderEdge {
 
     @Override
