beda41c5a4c73abd3c26ce7745554161ab210cf5
Harsh Shah <harshshah@NOVI-C02GH2N7MD6M.local>
2023-04-14 11:25:26 +0530

    Logic for finding edges added

--- src/main/java/com/hotstar/orchestration/service/Impl/PathFinderEdgeImpl.java
+++ src/main/java/com/hotstar/orchestration/service/Impl/PathFinderEdgeImpl.java
@@ -1,7 +1,11 @@
 package com.hotstar.orchestration.service.Impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import org.springframework.stereotype.Service;
 
@@ -12,7 +16,44 @@ public class PathFinderEdgeImpl implements PathFinderEdge {
 
     @Override
     public List<String> findEdgePath(Map<String, Map<String, String>> graph, String source, String destination) {
-       return null;
+        Queue<String> queue = new LinkedList<>();
+        Map<String,String> parent = new HashMap<>();
+        Map<String,String> edge = new HashMap<>();
+
+        queue.add(source);
+        parent.put(source,null);
+
+        while(!queue.isEmpty())
+        {
+            String current = queue.poll();
+            if(current.equals(destination)){
+                break;
+            }
+            for(Map.Entry<String,String> entry: graph.get(current).entrySet()){
+                String neighbor = entry.getKey();
+                if(!parent.containsKey(neighbor)){
+                    queue.add(neighbor);
+                    parent.put(neighbor,current);
+                    edge.put(neighbor,entry.getValue());
+                }
+            }
+        }
+
+        if(!parent.containsKey(destination)){
+            return null;
+        }
+
+        List<String> edges = new ArrayList<>();
+
+        String current = destination;
+        while(current!=null){
+            String parentOfCurrent = parent.get(current);
+            if(parentOfCurrent!=null){
+                edges.add(0, edge.get(current));
+            }
+            current = parentOfCurrent;
+        }
+        return edges;
     }
     
 }
