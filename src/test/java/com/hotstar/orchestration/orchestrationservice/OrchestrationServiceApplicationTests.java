e143120ac76efe443bb4e99c206dfcc327ec69e4
Harsh <harsh.shah.int@hotstar.com>
2023-04-30 12:38:09 +0530

    Fix Tests

--- src/test/java/com/hotstar/orchestration/orchestrationservice/OrchestrationServiceApplicationTests.java
+++ src/test/java/com/hotstar/orchestration/orchestrationservice/OrchestrationServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.hotstar.orchestration.orchestrationservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrchestrationServiceApplicationTests {
+public class OrchestrationServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
